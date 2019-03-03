d1825a919dc0047733b1a055be5fb10a4b11ea4c
gdemirev01 <gdemirev78@gmail.com>
2019-03-03 18:10:09 +0200

    user profile_pic

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,9 +1,6 @@
-class User < ApplicationRecord
+class View < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :reviews
-  has_one_attached :profile_pic
 end
