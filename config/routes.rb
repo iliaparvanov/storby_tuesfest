1236f0f28bee8b924cb251602ce51903193d8a1a
iliapa1 <ilia.nikolaev.parvanov@gmail.com>
2019-04-11 21:19:18 +0300

    Everything related to devise is now in /dev/ ; removed navbar from sign in/up pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :userrails
   get "/", to: "home#index"
   resources :home, :reviews
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, path: 'dev', controllers: { registrations: "users/registrations" }
   devise_for :views
   resources :games
   # patch "/games/:id/buy", to: "games#add_to_user"
